9dcf7474dd01a4837341854632d07c952803e584
SpOOk <spook.devv@gmail.com>
2023-06-26 18:35:34 -0400

    Completed state challenge

--- src/App.js
+++ src/App.js
@@ -2,26 +2,63 @@ import { useState } from "react";
 import "./styles.css";
 
 function App() {
-  return (
-    <div>
-      <p>
-        Edit <code>src/App.js</code> and save to reload.
-      </p>
-    </div>
-  );
+  return <Step />;
 }
 
 // Step component
 function Step() {
   // step variable
   const [step, setStep] = useState(1);
+  const [count, setCount] = useState(0);
+  const date = new Date("june 26 2023");
+  date.setDate(date.getDate() + count);
+
+  // subtract state value
+  function handleSubStep() {
+    setStep((s) => step - 1);
+  }
+
+  // add state value
+  function handleAddStep() {
+    setStep((s) => s + 1);
+  }
+
+  // subtract count
+  function handleSubCount() {
+    setCount((c) => c - step);
+  }
+
+  // add count
+  function handleAddCount() {
+    setCount((c) => c + step);
+  }
 
   return (
-    <div>
-      {/* Buttons */}
-      <button>-</button>
-      <p>Step: {step}</p>
-      <button>+</button>
+    <div className="App">
+      {/* When user clicks, step number - increase or decrease */}
+      {/* step div */}
+      <div>
+        <button onClick={handleSubStep}>-</button>
+        <span>Step: {step}</span>
+        <button onClick={handleAddStep}>+</button>
+      </div>
+      {/* count div */}
+      <div>
+        <button onClick={handleSubCount}>-</button>
+        <span>Count: {count}</span>
+        <button onClick={handleAddCount}>+</button>
+      </div>
+      {/* result div */}
+      <div>
+        <span>
+          {count === 0
+            ? "Today is "
+            : count > 0
+            ? `${count} days from today is `
+            : `${Math.abs(count)} days ago was `}
+        </span>
+        <span>{date.toDateString()}</span>
+      </div>
     </div>
   );
 }
